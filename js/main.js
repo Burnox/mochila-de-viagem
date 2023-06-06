9770e5b8ac64974483f4c2c805038aa939d8e575
Burnox <blu.uchoa@gmail.com>
2023-06-06 08:18:22 -0300

    .

--- js/main.js
+++ js/main.js
@@ -45,9 +45,9 @@ form.addEventListener('submit', evento => {
     itemAtual.id = existe.id
 
     atualizaItem(itemAtual)
-    itens[existe.id] = itemAtual
+    itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
   } else {
-    itemAtual.id = itens.length
+    itemAtual.id = itens[itens.length] - 1 ? itens[itens.length - 1].id + 1 : 0
 
     criaElemento(itemAtual)
 
@@ -71,9 +71,10 @@ function criaElemento(item) {
   const numeroItem = document.createElement('strong')
   numeroItem.innerHTML = item.quantidade
   numeroItem.dataset.id = item.id
-  novoItem.appendChild(numeroItem)
 
+  novoItem.appendChild(numeroItem)
   novoItem.innerHTML += item.nome
+  novoItem.appendChild(botaoDeleta(item.id))
 
   lista.appendChild(novoItem)
 }
@@ -81,4 +82,28 @@ function criaElemento(item) {
 function atualizaItem(item) {
   document.querySelector("[data-id='" + item.id + "']").innerHTML =
     item.quantidade
+}
+
+function botaoDeleta(id) {
+  const elementoBotao = document.createElement('button')
+  elementoBotao.innerText = 'X'
+
+  elementoBotao.addEventListener('click', function () {
+    deletaElemento(this.parentNode, id)
+  })
+
+  return elementoBotao
+}
+
+function deletaElemento(tag, id) {
+  tag.remove()
+
+  //remover um item do array
+  itens.splice(
+    itens.findIndex(elemento => elemento.id === id),
+    1
+  )
+
+  //escrever no localStorage
+  localStorage.setItem('itens', JSON.stringify(itens))
 }
